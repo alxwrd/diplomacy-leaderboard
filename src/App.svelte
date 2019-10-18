36e7245192c5f407c410a84e10aa1ee88cdac784
Alex Ward <alxwrd@googlemail.com>
2019-10-18 10:22:18 +0100

    update leaderboard

--- src/App.svelte
+++ src/App.svelte
@@ -13,24 +13,20 @@
     text-center block h-full">
     <Title />
 
-    <PlayerPosition position="1st" name="Adam" points="232" />
-    <PlayerPosition position="2nd" name="Alex" points="110" />
-    <PlayerPosition position="3rd" name="Jill" points="109" />
-    <PlayerPosition position="4th" name="Steven" points="46" />
+    <PlayerPosition position="1st" name="Adam" points="240" />
+    <PlayerPosition position="2nd" name="Jill" points="226" />
+    <PlayerPosition position="3rd" name="Alex" points="110" />
+    <PlayerPosition position="4th" name="Steven" points="49" />
     <PlayerPosition position="5th" name="Chris" points="17" />
     <PlayerPosition position="6th" name="Guillermo" points="2" />
-    <PlayerPosition position="7th" name="Dale" points="1" />
-    <PlayerPosition position="7th" name="James" points="1" />
+    <PlayerPosition position="6th" name="James" points="2" />
+    <PlayerPosition position="8th" name="Dale" points="1" />
     <PlayerPosition position="9th" name="Lewis" points="-1" />
   </div>
 
   <div class="w-auto lg:w-1/4 m-4 p-4 bg-gray-100 rounded shadow-lg">
     <div class="mb-4">
       <p class="text-3xl font-semibold">Upcoming Game</p>
-      <GameCard
-        title="War has changed"
-        image="https://i.imgur.com/AzWJ3zJ.png"
-        url="War-has-changed/5881352149270528/invite/LIL35K" />
 
     </div>
 
@@ -41,6 +37,10 @@
     <div class="mb-4">
       <p class="text-3xl font-semibold">Previous Games</p>
 
+      <GameCard
+        title="War has changed"
+        image="https://i.imgur.com/AzWJ3zJ.png"
+        url="War-has-changed/5881352149270528/invite/LIL35K" />
       <GameCard
         title="War never changes"
         image="https://i.imgur.com/8cJkBGy.png"
